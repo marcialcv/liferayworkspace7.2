65f24218f5d5fcc3386df496d0f37f77f557a7fb
marcialcalvo <marcialcalvo@192.168.1.36>
2020-05-06 18:45:23 +0200

    reordered

--- liferayworkspace/modules/journal-article-audit/src/main/java/journal/article/audit/constants/JournalArticleAuditConstants.java
+++ liferayworkspace/modules/journal-article-audit/src/main/java/journal/article/audit/constants/JournalArticleAuditConstants.java
@@ -12,7 +12,7 @@ public class JournalArticleAuditConstants {
 	public static final String ID_ATTR = "id";
 	public static final String GROUP_ID_ATTR = "groupId";
 	public static final String COMPANY_ID_ATTR = "companyId";
-	public static final String AUDIT_CLASSNAME = JournalArticle.class.getName();
 	public static final String IN_TRASH_ATTR = "inTrash";
+	public static final String AUDIT_CLASSNAME = JournalArticle.class.getName();
 
 }
