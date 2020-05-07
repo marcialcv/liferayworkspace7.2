78b4ff6527c45c56e7e5b64776557e03869d0f58
marcialcalvo <marcialcalvo@localhost>
2020-05-07 22:25:59 +0200

    audit events maintenance

---
+++ liferayworkspace/modules/maintenance-audit-storage/src/main/java/maintenance/audit/storage/ScheduledRemoveAuditEventTask.java
@@ -0,0 +1,116 @@
+package maintenance.audit.storage;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.SchedulerException;
+import com.liferay.portal.kernel.scheduler.StorageType;
+import com.liferay.portal.kernel.scheduler.StorageTypeAware;
+import com.liferay.portal.kernel.scheduler.Trigger;
+import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author marcialcalvo
+ *
+ */
+@Component(immediate = true, property = {
+		"cron.expression=00 00 00 L */3 ? *" }, service = ScheduledRemoveAuditEventTask.class)
+public class ScheduledRemoveAuditEventTask extends BaseMessageListener {
+
+	@Override
+	protected void doReceive(Message message) throws Exception {
+
+		_log.info("Executing Scheduled Task: RemovePastAuditEventsMessageListener");
+
+		maintenanceAuditEvents.removePastAuditEvents();
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+
+		String cronExpression = GetterUtil.getString(properties.get("cron.expression"), _DEFAULT_CRON_EXPRESSION);
+
+		String listenerClass = getClass().getName();
+		Trigger jobTrigger = _triggerFactory.createTrigger(listenerClass, listenerClass, new Date(), null,
+				cronExpression);
+
+		_schedulerEntryImpl = new SchedulerEntryImpl(getClass().getName(), jobTrigger);
+
+		if (_initialized) {
+			deactivate();
+		}
+
+		_schedulerEngineHelper.register(this, _schedulerEntryImpl, DestinationNames.SCHEDULER_DISPATCH);
+
+		_initialized = true;
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		if (_initialized) {
+			try {
+				_schedulerEngineHelper.unschedule(_schedulerEntryImpl, getStorageType());
+			} catch (SchedulerException se) {
+				if (_log.isWarnEnabled()) {
+					_log.warn("Unable to unschedule trigger", se);
+				}
+			}
+			_schedulerEngineHelper.unregister(this);
+		}
+		_initialized = false;
+	}
+
+	protected StorageType getStorageType() {
+		if (_schedulerEntryImpl instanceof StorageTypeAware) {
+			return ((StorageTypeAware) _schedulerEntryImpl).getStorageType();
+		}
+
+		return StorageType.MEMORY_CLUSTERED;
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(ModuleServiceLifecycle moduleServiceLifecycle) {
+		// With this reference, this component activation waits until portal
+		// initialization has completed.
+	}
+
+	@Reference(unbind = "-")
+	protected void setTriggerFactory(TriggerFactory triggerFactory) {
+		_triggerFactory = triggerFactory;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSchedulerEngineHelper(SchedulerEngineHelper schedulerEngineHelper) {
+		_schedulerEngineHelper = schedulerEngineHelper;
+	}
+
+	@Reference
+	protected MaintenanceAuditEventsUtil maintenanceAuditEvents;
+
+	// At 00:00:00am, on the last day of the month, every 3 months starting in
+	// January
+	private static final String _DEFAULT_CRON_EXPRESSION = "00 00 00 L */3 ? *";
+
+	private static final Log _log = LogFactoryUtil.getLog(ScheduledRemoveAuditEventTask.class);
+
+	private volatile boolean _initialized;
+	private TriggerFactory _triggerFactory;
+	private SchedulerEngineHelper _schedulerEngineHelper;
+	private SchedulerEntryImpl _schedulerEntryImpl = null;
+}
